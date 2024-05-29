c07ca7076ecfde607afd941fa9bf8e913aa65eed
Mehul Agarwal <agarwalmehul92@gmail.com>
2024-05-29 11:47:44 +0530

    Docs Updated

--- docs/js/menu-wc.js
+++ docs/js/menu-wc.js
@@ -15,7 +15,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">PinpointSdk</a>
+                    <a href="index.html" data-type="index-link">ApiCrypto</a>
                 </li>
 
                 <li class="divider"></li>
